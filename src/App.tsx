f7ede05a4d34a60aef8446f683b7d035377b0434
Ben Burgess <benburgess519@gmail.com>
2025-05-03 11:18:03 -0500

    chore: add Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import UserService from "./services/userService";
+import React from "react";
 
 function App() {
+  const userService = new UserService();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          onMouseEnter={() => userService.verify()}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
---
+++ src/services/userService.ts
@@ -0,0 +1,9 @@
+export default class UserService {
+  constructor() {}
+
+  sendCode() {}
+
+  verify() {
+    console.log("verify");
+  }
+}
