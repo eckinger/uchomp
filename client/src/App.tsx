3f3f07ec505ad4a60c8bd75104c6bf42bcbd3238
Ben Burgess <benburgess519@gmail.com>
2025-05-05 16:22:41 -0500

    fix(routes): remove /groups

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,7 @@ import User from "./models/user";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/groups" replace />} />
-      <Route path="/groups" element={<Navigate to="/groups/view" replace />} />
+      <Route path="/" element={<Navigate to="/groups/view" replace />} />
       <Route path="/groups/view" element={<ViewGroups />} />
       <Route path="/groups/create" element={<CreateGroup />} />
       <Route path="/groups/records" element={<RecordInfo />} />
